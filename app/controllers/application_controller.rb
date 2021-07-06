fddca358cb2efa243f08ac53cdbe9cb0832ce743
Daniel <cdanielask@gmail.com>
2021-07-06 13:13:45 +1000

    Adds cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,13 @@ class ApplicationController < ActionController::API
   end
 
   def authenticated
-    token = request.authorization.split(' ')[1]
-    decoded = JWT.decode(token, Rails.application.credentials.jwt_secret_key, true, algorithm: 'HS512')
-    user_id = decoded[0]['user_id']
-    @user = User.find(user_id)
-
-    render json: { message: 'Unauthorized: Please log in' } unless @user
+    begin
+      token = request.authorization.split(' ')[1]
+      decoded = JWT.decode(token, Rails.application.credentials.jwt_secret_key, true, algorithm: 'HS512')
+      user_id = decoded[0]['user_id']
+      @user = User.find(user_id)
+    rescue => exception
+      render json: { message: 'Unauthorized: Please log in' }
+    end
   end
 end
